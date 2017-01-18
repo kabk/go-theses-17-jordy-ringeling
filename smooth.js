c18f8514ddb5f79328df4e69156f4ec5aedda8df
XxxCodeSlayer93xxX <jordy.ringeling@gmail.com>
2017-01-18 15:17:52 +0100

    update

--- smooth.js
+++ smooth.js
@@ -7,7 +7,7 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top -30
+          scrollTop: target.offset().top -29
         }, 500);
         return false;
       }
